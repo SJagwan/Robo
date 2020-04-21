0ddc6031ec01720c7bdfe16ee0e9ee13b4f93c67
SJagwan <shubhanshu1997jagwan@gmail.com>
2020-04-21 15:59:32 +0530

    Updates

---
+++ precache-manifest.e8fb92c05be322686912ebd54b1ce95a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e5746e7369ea1f920c1bfecd9109563",
+    "url": "/Robo/index.html"
+  },
+  {
+    "revision": "462e0d6f658c0fc1db68",
+    "url": "/Robo/static/css/main.b57440d8.chunk.css"
+  },
+  {
+    "revision": "eeced4b7b62b9a1e280d",
+    "url": "/Robo/static/js/2.1a11e23f.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/Robo/static/js/2.1a11e23f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "462e0d6f658c0fc1db68",
+    "url": "/Robo/static/js/main.4a3042b4.chunk.js"
+  },
+  {
+    "revision": "3375fd9ca7b2e7d488c1",
+    "url": "/Robo/static/js/runtime-main.d80cd92f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Robo/static/media/logo.5d5d9eef.svg"
+  }
+]);
